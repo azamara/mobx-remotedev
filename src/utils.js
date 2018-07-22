d33ef5ebfa0052fe2841d700f29ebe23773a40a1
William Kim <azamara@me.com>
2018-07-22 17:38:12 +0900

    support mobx 4.x

--- src/utils.js
+++ src/utils.js
@@ -30,7 +30,8 @@ export function createAction(name, change) {
 
 export function getName(obj) {
   if (!obj || !mobx.isObservable(obj)) return '';
-  let r = mobx.extras.getDebugName(obj);
+  let r = mobx.extras ? mobx.extras.getDebugName(obj) : mobx.getDebugName(obj);
+
   let end = r.indexOf('.');
   if (end === -1) end = undefined;
   return r.substr(0, end);
